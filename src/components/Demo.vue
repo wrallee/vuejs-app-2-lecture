e8942f2a55baf1d4e90f8d018a11c0f232e86d94
wrallee <wrallee@naver.com>
2020-06-01 22:21:10 +0900

    ex2: The simplicity of helper functions

---
+++ src/components/Demo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="root">
+      <p>{{ originalPrice }}</p>
+      <p>{{ doublePrice }}</p>
+      <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    computed: {
+        ...mapGetters(['originalPrice', 'doublePrice', 'triplePrice'])
+    }
+    // 위 코드 == 아래 코드: 헬퍼 함수가 주는 간편함~
+    // computed: {
+    //     originalPrice() {
+    //         return this.$store.getters.originalPrice;
+    //     },
+    //     doublePrice() {
+    //         return this.$store.getters.doublePrice;
+    //     },
+    //     triplePrice() {
+    //         return this.$store.getters.triplePrice;
+    //     }
+    // }
+}
+</script>
+
+<style>
+
+</style>
